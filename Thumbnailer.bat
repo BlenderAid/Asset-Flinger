9a34dcde2cfabb39cdff6ebd9d49b5341ce9ed7b
Manu Järvinen <manu.jarvinen@gmail.com>
2014-12-30 16:06:23 +0200

    Update Thumbnailer.bat

--- Thumbnailer.bat
+++ Thumbnailer.bat
@@ -1,1 +1,1 @@
-@for %%i in (%*) do "C:\Program Files\Blender\blender-app.exe" -b "%~dp0/.thumbnailer_METAL/Thumbnailer.blend" --python-text ThumbScript --objFiles %%i
+@for %%i in (%*) do "C:\Program Files\Blender Foundation\Blender\blender-app.exe" -b "%~dp0/.thumbnailer_MTL/Thumbnailer.blend" --python-text ThumbScript --objFiles %%i
